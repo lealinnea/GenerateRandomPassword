efdc1207b5d7d573f5f779ff44c9387b5273d5d2
lealinnea <leag0905@gmail.com>
2022-03-02 13:13:41 -0600

    Adjustments

---
+++ readme.md
@@ -0,0 +1,11 @@
+# Genertating Password
+
+## Link to portfolio
+https://github.com/lealinnea/GenerateRandomPassword
+## Purpose 
+The purpose of the generating password is to learn about confirms and how to exicute and logging a random password with an event listener
+
+## Screenshot
+
+## Created By:
+Lea Guerrero
--- script.js
+++ script.js
@@ -11,70 +11,84 @@ function writePassword() {
 }
 
 function generatePassword() {
+// using parseInt so it will return an int 
+  var passwordNum = parseInt(
+    prompt('How many characters would you like your password to contain (8-128)?')
+  );
+  console.log(passwordNum)
   // prompt number of characters between 8- 128
-  var passwordNum = prompt("Enter a number between 8-128");
+  // var passwordNum = prompt("Enter a number between 8-128");
   //This resolves the users error when they type a value outside of 8-128
   if (passwordNum < 8 || passwordNum > 128) {
-    alert("Please enter valid number between 8 - 128");
-  }
-  //confirms- asking user a yes or no question which would result in a true false
- 
-  //upper case
-  var isUpperCase = confirm("Do you want upper case?")
-  console.log(isUpperCase)
-  //lower case
-  var isLowerCase = confirm("Do you want lower case?")
-  console.log(isLowerCase)
-  //numbers
-  var isNumber = confirm("Do you want numbers?")
-  console.log(isNumber)
-  //special characters
-  var isSpecialCharacter = confirm("Do you want special characters?")
-  console.log(isSpecialCharacter)
+    alert("Please enter valid number between 8 - 128")
+    return null;
+  } else {
 
 
-//splitting the characters so they stand alone
-  var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
-  console.log(upperCase)
-  var lowerCase = "abcdefghijklmnopqrstuvwxyx".split("");
-  console.log(lowerCase)
-  var numbers = "1234567890".split("");
-  console.log(numbers)
-  var specialCharacter = "~`!@#$%^&*()_-+<>?+={}[]|:;',.".split("");
-  console.log(specialCharacter)
-  
-  //creating a variable to hold new password
-  var newUserPassword = "";
+    //confirms- asking user a yes or no question which would result in a true false
+    //upper case
+    var isUpperCase = confirm("Do you want upper case?");
+    console.log(isUpperCase);
+    //lower case
+    var isLowerCase = confirm("Do you want lower case?");
+    console.log(isLowerCase)
+    //numbers
+    var isNumber = confirm("Do you want numbers?");
+    console.log(isNumber)
+    //special characters
+    var isSpecialCharacter = confirm("Do you want special characters?");
+    console.log(isSpecialCharacter)
 
-// while loop is allowing it to be run many times
-//new user password has to be less than or equal to passwordNum which declaired above is a number between 8-128 
-//declairing i as a new value which is adding the length of the string-which turned into an arrray due to split function
-//adding new user password + uppercase array to get a value for each one
-  while (newUserPassword.length <= passwordNum)
-    if (upperCase) {
-      i = genRandom(upperCase.length);
-      newUserPassword = newUserPassword + upperCase[i];
-  }
-  if (lowerCase) {
-      i = genRandom(lowerCase.length);
-      newUserPassword = newUserPassword + lowerCase[i];
-  }
-   if (numbers) {
-      i = genRandom(numbers.length);
-      newUserPassword = newUserPassword + numbers[i];
-   }
-    if (specialCharacter) {
-      i = genRandom(specialCharacter.length);
-      newUserPassword = newUserPassword + specialCharacter[i];
+
+    //declaring total options for password for each variable
+    var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    console.log(upperCase)
+    var lowerCase = "abcdefghijklmnopqrstuvwxyx";
+    console.log(lowerCase)
+    var numbers = "1234567890";
+    console.log(numbers)
+    var specialCharacter = "~`!@#$%^&*()_-+<>?+={}[]|:;',.";
+    console.log(specialCharacter)
+
+
+ //creating a variable to hold the total new password options
+    var passwordOptions = '';
+
+    //if isUpperCase is true then enter the conditional and add all of the uppercase options to passwordOptions
+    if (isUpperCase) {
+      passwordOptions += upperCase;
+    }
+    //if isLowerCase is true then enter the conditional and add all of the lowercase options to passwordOptions
+    if (isLowerCase) {
+      passwordOptions += lowerCase;
+    }
+    //if isNumber is true then enter the conditional and add all of the number options to passwordOptions
+    if (isNumber) {
+      passwordOptions += numbers;
+    }
+    //if isSpecialCharacter is true then enter the conditional and add all of the special character options to passwordOptions
+    if (isSpecialCharacter) {
+      passwordOptions += specialCharacter;
+    }
+
+    console.log(passwordOptions);
+
+
+    //setting the new user password to empty string to add to in the for loop
+    var newUserPass = '';
+
+    //looping over total character options and randomizing the options based on index. This will go until it reaches the desired length of password
+    for (var i = 0; i < passwordNum; i++) {
+      newUserPass += passwordOptions[Math.floor(Math.random() * passwordOptions.length)];
+    }
+    console.log(newUserPass);
   }
   //stopping funtion and retruning a value in this case a new random password
-       console.log(newUserPassword);
-          return newUserPassword
-  }
+  return newUserPass;
 
-  function genRandom(max){
-    //math floor rounding the number is in the () to the largest integer value and since there is a math.random that is genrating a random value x our arrays
-  return Math.floor(Math.random() * max);
-  }
-  // Add event listener to generate button -- on click it creates a password
-  generateBtn.addEventListener("click", writePassword);
+}
+
+
+
+// Add event listener to generate button -- on click it creates a password
+generateBtn.addEventListener("click", writePassword);
